b48e2e94bf2c95da62bc5d1937c09323e3cf1edf
Luke Simkins <lucas.simkins@gmail.com>
2017-11-06 15:09:19 -0600

    Update style url.

--- src/map/Map.component.tsx
+++ src/map/Map.component.tsx
@@ -24,39 +24,24 @@ export class Map extends React.Component {
 
   state = {
     center: {
-      latitude: 40.72052634,
-      longitude: -73.97686958312988
+      latitude: 35.487868,
+      longitude: -97.549959
     },
     zoom: 11,
     currentZoom: null,
     userTrackingMode: Mapbox.userTrackingMode.none,
     annotations: [{
-      coordinates: [40.72052634, -73.97686958312988],
-      type: 'point',
-      title: 'This is marker 1',
-      subtitle: 'It has a rightCalloutAccessory too',
-      rightCalloutAccessory: {
-        source: { uri: 'https://cldup.com/9Lp0EaBw5s.png' },
-        height: 25,
-        width: 25
-      },
-      annotationImage: {
-        source: { uri: 'https://cldup.com/CnRLZem9k9.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker1'
-    }, {
-      coordinates: [40.714541341726175,-74.00579452514648],
+      coordinates: [35.487868,-97.549959],
       type: 'point',
       title: 'Important!',
       subtitle: 'Neat, this is a custom annotation image',
+      fillColor: '#0000ff',
+      id: 'marker2',
       annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
+        source: { uri: 'https://maxcdn.icons8.com/Share/icon/City//gps_device1600.png' },
         height: 25,
         width: 25
       },
-      id: 'marker2'
     }, {
       coordinates: [[40.76572150042782,-73.99429321289062],[40.743485405490695, -74.00218963623047],[40.728266950429735,-74.00218963623047],[40.728266950429735,-73.99154663085938],[40.73633186448861,-73.98983001708984],[40.74465591168391,-73.98914337158203],[40.749337730454826,-73.9870834350586]],
       type: 'polyline',
@@ -131,7 +116,7 @@ export class Map extends React.Component {
         'coordinates': [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
         'type': 'polygon',
         'fillAlpha': 1,
-        'fillColor': '#000000',
+        'fillColor': '#ffffff',
         'strokeAlpha': 1,
         'id': 'new-black-polygon'
       }]
@@ -179,7 +164,7 @@ export class Map extends React.Component {
           scrollEnabled={true}
           zoomEnabled={true}
           showsUserLocation={false}
-          styleURL={Mapbox.mapStyles.dark}
+          styleURL={"mapbox://styles/spectracks/cj9k5y6m70dqc2rt3soie8dx3"}
           userTrackingMode={this.state.userTrackingMode}
           annotations={this.state.annotations}
           annotationsAreImmutable
